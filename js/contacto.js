2ff577360b200c2ce643835d892b0be6e1adf278
Francisco Ezequiel Luna <96634021+franciscoeluna@users.noreply.github.com>
2023-11-14 11:48:08 -0300

    Add files via upload

---
+++ js/contacto.js
@@ -0,0 +1,28 @@
+// VALIDACIÓN-CONTACTO
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+
+FormData.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+
+    if(nombre.value.length < 5){
+        warnings += `El nombre no es valido`
+        entrar = true
+    }
+
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido`
+        entrar = true
+    }
+
+    if (entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    }
+})
+
